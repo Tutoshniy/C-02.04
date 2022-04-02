0960e98ecfb9a1eaf2fa96631441126c56042b13
Gleb <gleb.gadbrat@mail.ru>
2022-04-02 21:06:42 +0300

    Задание 3 - Показать двумерный массив размером m×n заполненный целыми числами

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество строк:" );
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:" );
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [m,n];
+void FillArray(int [,] mat){
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            mat[i,j] = new Random().Next(1,101);
+        }
+    }
+}
+void PrintArray(int [,] mat){
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{mat[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(matrix);
+PrintArray(matrix);
